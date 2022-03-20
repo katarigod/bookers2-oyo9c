27f35ba588d2774ff3edb5168e006f5aae9e8ce5
katarigod <98676007+katarigod@users.noreply.github.com>
2022-03-20 03:07:36 +0000

    Work Finish

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  has_many :favorited_books, through: :favorites, source: :books
 
 
 
